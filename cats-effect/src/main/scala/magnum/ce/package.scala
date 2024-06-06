988d44944bf19a6d40e8969893c4212add13284b
johannes karoff <johannes@karoff.net>
2024-06-06 23:09:23 +0200

    make connectF/transactF usable

--- cats-effect/src/main/scala/magnum/ce/package.scala
+++ cats-effect/src/main/scala/magnum/ce/package.scala
@@ -7,17 +7,24 @@ import com.augustnagro.magnum.{DbCon, DbTx, SqlException}
 import java.sql.Connection
 import javax.sql.DataSource
 
-def connectF[F[_]: Sync, T](dataSource: DataSource)(f: DbCon ?=> F[T]): F[T] =
-  val connection = Resource.fromAutoCloseable(Sync[F].blocking(dataSource.getConnection()))
-  connection.use(con => f(using DbCon(con)))
+inline def connectF[F[_]: Sync] = new PartiallyAppliedConnectF
 
-def transactF[F[_]: Sync, T](dataSource: DataSource, connectionConfig: Connection => F[Unit])(f: DbTx ?=> F[T]): F[T] =
-  val connection = Resource.fromAutoCloseable(Sync[F].blocking(dataSource.getConnection()))
-  connection.use { con =>
-    connectionConfig(con) *>
-      Sync[F].delay(con.setAutoCommit(false)) *>
-      f(using DbTx(con)).flatTap(_ => Sync[F].blocking(con.commit())).onError(_ => Sync[F].blocking(con.rollback()))
-  }
+inline def transactF[F[_]: Sync] = new PartiallyAppliedTransactF
 
-def transactF[F[_]: Sync, T](dataSource: DataSource)(f: DbTx ?=> F[T]): F[T] =
-  transactF(dataSource, _ => Sync[F].unit)(f)
+class PartiallyAppliedConnectF[F[_]: Sync] {
+  def apply[F[_]: Sync, T](dataSource: DataSource)(f: DbCon ?=> F[T]): F[T] =
+    val connection = Resource.fromAutoCloseable(Sync[F].blocking(dataSource.getConnection()))
+    connection.use(con => f(using DbCon(con)))
+}
+
+class PartiallyAppliedTransactF[F[_]: Sync] {
+  def apply[T](dataSource: DataSource, connectionConfig: Connection => F[Unit])(f: DbTx ?=> F[T]): F[T] =
+    val connection = Resource.fromAutoCloseable(Sync[F].blocking(dataSource.getConnection()))
+    connection.use { con =>
+      connectionConfig(con) *>
+        Sync[F].delay(con.setAutoCommit(false)) *>
+        f(using DbTx(con)).flatTap(_ => Sync[F].blocking(con.commit())).onError(_ => Sync[F].blocking(con.rollback()))
+    }
+
+  def apply[T](dataSource: DataSource)(f: DbTx ?=> F[T]): F[T] = apply(dataSource, _ => Sync[F].unit)(f)
+}
